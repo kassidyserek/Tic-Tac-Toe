7be756947cd89da4e04366a7156b228744eb09be
Kassidy <kqssidy@gmail.com>
2021-10-27 10:41:01 -0500

    Added checkForWinner function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -30,20 +32,33 @@ struct ContentView: View {
                     .cornerRadius(30)
                     .onTapGesture {
                         withAnimation(Animation.default) {
-                        if moves[index] == "" {
-                            moves[index] = xTurn ? "X" : "O"
-                            xTurn.toggle()
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
                         }
                     }
-                }
                     .rotation3DEffect(
                         .init(degrees: moves[index] != "" ? 180: 0),
                         axis: (x: 0.0, y: 1.0, z: 0.0)
                     )
-            }
+                }
             })
-    }        .preferredColorScheme(.dark)
-}
+        }
+        .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver, content: {
+            Alert(title: Text(winMessage))
+        })
+        .onChange(of: moves, perform: {value in
+            checkForWinner()
+        })
+    }
+    private func checkForWinner() {
+        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2] {
+            winMessage = "\(moves[0]) is the winner!"
+            gameOver = true
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
